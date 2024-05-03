23f3bddd33ff7a7569d3ff34e5e99efd19d53743
Udhayajsk <udhayanithi3024@gmail.com>
2024-05-03 15:21:59 +0530

    initial

---
+++ Routers/Hall.routers.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  AllCustomerBookedData,
+  BookRoom,
+  BookedRoomData,
+  CreateRoom,
+  RoomDetail,
+  customerBookingDetails,
+} from "../Controllers/Hall.controllers.js";
+
+const router = express.Router();
+
+router.get("/allroomdetails", RoomDetail);
+router.post("/createroom", CreateRoom);
+router.post("/bookingroom", BookRoom);
+router.get("/bookedroomdata", BookedRoomData);
+router.get("/allcustomerbookeddata", AllCustomerBookedData);
+router.get("/customerbookingcount", customerBookingDetails);
+
+export default router;
